dd01b801be9a80c7b89b9f9fee63d79769d9cb85
Shigeki Fujimoto <shigeki19_firesign@me.com>
2022-05-15 02:13:32 +0900

    ビルドで消えるのでnext-env.d.tsファイルを分割

---
+++ monogatari.d.ts
@@ -0,0 +1,22 @@
+declare namespace JSX {
+    interface IntrinsicElements {
+        // mongatariで使用するタグ
+        'visual-novel': any,
+        'language-selection-screen': any,
+        'loading-screen': any,
+        'main-screen': any,
+        'main-menu': any,
+        'game-screen': any,
+        'dialog-log': any,
+        'text-box': any,
+        'quick-menu': any,
+        'gallery-screen': any,
+        'credits-screen': any,
+        'load-screen': any,
+        'save-screen': any,
+        'settings-screen': any,
+        'help-screen': any
+    }
+};
+
+declare module '@monogatari/core';
